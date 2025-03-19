5a9eca9b5fca90656dc996271c82479d7358b57b
ppxxp <1431292416@qq.com>
2025-03-19 16:28:28 +0800

    huaweicloud - add TagCountFilter

--- tools/c7n_huaweicloud/c7n_huaweicloud/actions/tms.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/actions/tms.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import logging
 from logging import exception
+from pydoc import render_doc
 
 from huaweicloudsdkcore.exceptions import exceptions
 from huaweicloudsdkiam.v3 import KeystoneListProjectsRequest
@@ -103,11 +104,11 @@ class CreateResourceTagAction(HuaweiCloudBaseAction):
         for failed_resource in failed_resources:
             resources.remove(failed_resource)
 
-    def process_resource_set(self, client, resource_batch, tag_batch, project_id):
-        request_body = ReqCreateTag(project_id=project_id, resources=resource_batch, tags=tag_batch)
+    def process_resource_set(self, client, resource_batch, tags, project_id):
+        request_body = ReqCreateTag(project_id=project_id, resources=resource_batch, tags=tags)
         request = CreateResourceTagRequest(body=request_body)
         client.create_resource_tag(request=request)
-        self.log.info("Successfully tagged %s resources with %s tags", len(resource_batch), len(tag_batch))
+        self.log.info("Successfully tagged %s resources with %s tags", len(resource_batch), len(tags))
 
     def get_project_id(self):
         iam_client = local_session(self.manager.session_factory).client("iam")
@@ -192,11 +193,112 @@ class DeleteResourceTagAction(HuaweiCloudBaseAction):
         for failed_resource in failed_resources:
             resources.remove(failed_resource)
 
-    def process_resource_set(self, client, resource_batch, tag_batch, project_id):
-        request_body = ReqDeleteTag(project_id=project_id, resources=resource_batch, tags=tag_batch)
+    def process_resource_set(self, client, resource_batch, tags, project_id):
+        request_body = ReqDeleteTag(project_id=project_id, resources=resource_batch, tags=tags)
         request = DeleteResourceTagRequest(body=request_body)
         client.delete_resource_tag(request=request)
-        self.log.info("Successfully remove tag %s resources with %s tags", len(resource_batch), len(tag_batch))
+        self.log.info("Successfully remove tag %s resources with %s tags", len(resource_batch), len(tags))
+
+    def get_project_id(self):
+        iam_client = local_session(self.manager.session_factory).client("iam")
+
+        region = local_session(self.manager.session_factory).region
+        request = KeystoneListProjectsRequest(name=region)
+        response = iam_client.keystone_list_projects(request=request)
+        for project in response.projects:
+            if (region == project.name):
+                return project.id
+
+        self.log.error("Can not get project_id for %s", region)
+        raise PolicyExecutionError("Can not get project_id for %s", region)
+
+
+class RenameResourceTagAction(HuaweiCloudBaseAction):
+    """Rename the specified tags from the specified resources.
+
+    :example:
+
+        .. code-block :: yaml
+
+            policies:
+            - name: multiple-rename-tag-example
+              resource: huaweicloud.volume
+              filters:
+                - type: value
+                  key: metadata.__system__encrypted
+                  value: "0"
+              actions:
+                - type: rename-tag
+                  value: 123
+                  old_key: owner-old
+                  new_key: owner-new
+    """
+
+    log = logging.getLogger("custodian.huaweicloud.actions.RenameResourceTagAction")
+
+    schema = type_schema("rename-tag",
+                         value={'type': 'string'},
+                         old_key={'type', 'string'},
+                         new_key={'type', 'string'})
+    resource_max_size = 50
+
+    def validate(self):
+        """validate"""
+        if not self.data.get('value'):
+            raise PolicyValidationError("Can not perform rename tag without value")
+        if not self.data.get('old_key'):
+            raise PolicyValidationError("Can not perform rename tag without old_key")
+        if not self.data.get('new_key'):
+            raise PolicyValidationError("Can not perform rename tag without new_key")
+        return self
+
+    def process(self, resources):
+        project_id = self.get_project_id()
+
+        value = self.data.get('value')
+        old_key = self.data.get('old_key')
+        new_key = self.data.get('new_key')
+
+        old_tags = [{"key": old_key, "value": value}]
+        new_tags = [{"key": new_key, "value": value}]
+
+        tms_client = self.get_tag_client()
+        resources = self.filter_resources(resources)
+        resources = [{"resource_id": resource["id"], "resource_type": resource["tag_type"]}
+                     for resource in resources
+                     if "tag_type" in resource.keys() and len(resource['tag_type']) > 0]
+
+        for resource_batch in chunks(resources, self.resource_max_size):
+            try:
+                self.process_resource_set(tms_client, resource_batch, old_tags, new_tags, project_id)
+            except exceptions.ClientRequestException as ex:
+                self.log.exception(
+                    f"Unable to rename tag {len(resource_batch)} resources RequestId: {ex.request_id}, Reason: {ex.error_msg}")
+                self.handle_exception(failed_resources=resource_batch)
+        return self.process_result(resources=resources)
+
+    def perform_action(self, resource):
+        pass
+
+    def filter_resources(self, resources):
+        old_key = self.data.get('old_key')
+        return [resource for resource in resources if old_key in resource.get('tags')]
+
+    def handle_exception(self, failed_resources, resources):
+        self.failed_resources.extend(failed_resources)
+        for failed_resource in failed_resources:
+            resources.remove(failed_resource)
+
+    def process_resource_set(self, client, resource_batch, old_tags, new_tags, project_id):
+        request_body = ReqDeleteTag(project_id=project_id, resources=resource_batch, tags=old_tags)
+        request = DeleteResourceTagRequest(body=request_body)
+        client.delete_resource_tag(request=request)
+        self.log.info("Successfully remove tag %s resources with %s tags", len(resource_batch), len(old_tags))
+
+        request_body = ReqCreateTag(project_id=project_id, resources=resource_batch, tags=new_tags)
+        request = CreateResourceTagRequest(body=request_body)
+        client.create_resource_tag(request=request)
+        self.log.info("Successfully tagged %s resources with %s tags", len(resource_batch), len(new_tags))
 
     def get_project_id(self):
         iam_client = local_session(self.manager.session_factory).client("iam")
