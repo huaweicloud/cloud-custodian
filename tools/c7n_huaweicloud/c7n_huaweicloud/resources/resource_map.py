31a19110565d79a34f9f88ff1985c3bddc068bd3
lizhaosheng <withfeelings@163.com>
2025-03-24 20:47:35 +0800

    Merge branch 'main' of https://github.com/huaweicloud/cloud-custodian into huaweicloud-main
    # Conflicts:
    #	tools/c7n_huaweicloud/c7n_huaweicloud/client.py
    #	tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
    #	tools/c7n_huaweicloud/pyproject.toml

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
@@ -4,7 +4,7 @@
 ResourceMap = {
     "huaweicloud.volume": "c7n_huaweicloud.resources.volume.Volume",
     "huaweicloud.deh": "c7n_huaweicloud.resources.deh.Deh",
-    "huaweicloud.config-tracker": "c7n_huaweicloud.resources.config.ConfigTracker"
+    "huaweicloud.config-tracker": "c7n_huaweicloud.resources.config.ConfigTracker",
     "huaweicloud.elb_listener": "c7n_huaweicloud.resources.elb.Listener",
     "huaweicloud.elb_loadbalancer": "c7n_huaweicloud.resources.elb.Loadbalancer",
     "huaweicloud.elb.loadbalancer": "c7n_huaweicloud.resources.elb.Loadbalancer",
