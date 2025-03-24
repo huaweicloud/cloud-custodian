a681b80906bb0a4583e601323459c441e1f196a5
yufanxuan <yufanxuan@126.com>
2025-03-24 14:17:50 +0800

    huaweicloud - override handler for functiongraph and fix some bugs

--- tools/c7n_huaweicloud/c7n_huaweicloud/handler.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/handler.py
@@ -7,13 +7,17 @@ import os
 import uuid
 
 from c7n.config import Config
-from c7n.loader import PolicyLoader
+from c7n.policy import PolicyCollection
+from c7n.resources import load_resources
+from c7n.structure import StructureParser
+from c7n.utils import reset_session_cache
 # Load resource plugins
 from c7n_huaweicloud.entry import initialize_huaweicloud
+from c7n_huaweicloud.provider import HuaweiCloud
 
 initialize_huaweicloud()
 
-log = logging.getLogger('custodian.gcp.functions')
+log = logging.getLogger('custodian.huaweicloud.functions')
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -27,21 +31,27 @@ def run(event, context=None):
         log.error('Invalid policy config')
         return False
 
-    # setup execution options
-    options = Config.empty(**policy_config.pop('execution-options', {}))
-    options.update(
-        policy_config['policies'][0].get('mode', {}).get('execution-options', {}))
-    # if output_dir specified use that, otherwise make a temp directory
-    if not options.output_dir:
-        options['output_dir'] = get_tmp_output_dir()
+    options_overrides = \
+        policy_config['policies'][0].get('mode', {}).get('execution-options', {})
 
-    loader = PolicyLoader(options)
-    policies = loader.load_data(policy_config, 'config.json', validate=False)
+    # if output_dir specified use that, otherwise make a temp directory
+    if 'output_dir' not in options_overrides:
+        options_overrides['output_dir'] = get_tmp_output_dir()
+
+    # merge all our options in
+    options = Config.empty(**options_overrides)
+
+    load_resources(StructureParser().get_resource_types(policy_config))
+
+    options = HuaweiCloud().initialize(options)
+    policies = PolicyCollection.from_data(policy_config, options)
     if policies:
         for p in policies:
             log.info("running policy %s", p.name)
             p.validate()
             p.push(event, context)
+
+    reset_session_cache()
     return True
 
 
