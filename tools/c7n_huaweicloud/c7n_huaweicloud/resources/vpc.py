647fc2d338e64bc815569aa4fde37f61c051fdd5
Hector-Jing <hcjing4596@outlook.com>
2025-03-29 09:37:47 +0800

    delete vpc tag_resource_type

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/vpc.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/vpc.py
@@ -40,7 +40,7 @@ class Vpc(QueryResourceManager):
         service = 'vpc_v2'
         enum_spec = ('list_vpcs', 'vpcs', 'marker')
         id = 'id'
-        tag_resource_type = 'vpcs'
+        tag_resource_type = ''
 
 
 @resources.register('vpc-port')
