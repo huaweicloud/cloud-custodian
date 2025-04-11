bd03d66c24194737eb5e3524829935868477814c
franklwy <39915330+franklwy@users.noreply.github.com>
2025-04-11 10:22:10 +0800

    Feature/dns (#9)
    add dns actions, filters, UTs

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
@@ -41,5 +41,8 @@ ResourceMap = {
     "huaweicloud.org-unit": "c7n_huaweicloud.resources.organizations.OrgUnit",
     "huaweicloud.ram-shared-principals": "c7n_huaweicloud.resources.ram.RAMSharedPrincipals",
     "huaweicloud.antiddos-eip": "c7n_huaweicloud.resources.antiddos.Eip",
-    "huaweicloud.kafka": "c7n_huaweicloud.resources.kafka.Kafka"
+    "huaweicloud.kafka": "c7n_huaweicloud.resources.kafka.Kafka",
+    "huaweicloud.dns-publiczone": "c7n_huaweicloud.resources.dns.PublicZone",
+    "huaweicloud.dns-privatezone": "c7n_huaweicloud.resources.dns.PrivateZone",
+    "huaweicloud.dns-recordset": "c7n_huaweicloud.resources.dns.RecordSet"
 }
