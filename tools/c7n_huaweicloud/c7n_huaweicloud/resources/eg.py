b1b24211db4902a7b91977e637a49fae796dbe7a
zyw0599 <zhouyanwu5@huawei.com>
2025-04-25 15:28:45 +0800

    feat:remove test action tag; augment tag

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/eg.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/eg.py
@@ -3,34 +3,30 @@
 
 import logging
 from huaweicloudsdkcore.exceptions import exceptions
-from huaweicloudsdkeg.v1 import (
-    ListEventStreamingRequest,
-    ShowEventStreamingRequest
+from huaweicloudsdktms.v1 import (
+    ShowResourceTagRequest
 )
 
 from c7n_huaweicloud.provider import resources
 from c7n_huaweicloud.query import QueryResourceManager, TypeInfo
-from c7n_huaweicloud.actions.base import HuaweiCloudBaseAction
-from c7n.utils import type_schema
-from c7n.filters import ValueFilter, Filter
 from c7n.filters.core import AgeFilter
-from c7n.utils import local_session
-from c7n_huaweicloud.filters.tms import TagActionFilter, TagCountFilter
+from c7n.utils import local_session, type_schema
 
 log = logging.getLogger('custodian.huaweicloud.eg')
 
 @resources.register('eventstreaming')
 class EventStreaming(QueryResourceManager):
-    """华为云EventGrid事件流资源管理器
-    
+    """Huawei Cloud EventGrid EventStreaming Resource Manager.
+
     :example:
 
     .. code-block:: yaml
 
         policies:
-          - name: event-streaming
+          - name: event-streaming-policy
             resource: huaweicloud.eventstreaming
-            region: cn-north-4  # EventGrid服务仅支持cn-east-2, cn-east-3, cn-north-4区域
+            # EventGrid service is only available in cn-east-2, cn-east-3, cn-north-4
+            region: cn-north-4 
     """
     
     class resource_type(TypeInfo):
@@ -41,35 +37,56 @@ class EventStreaming(QueryResourceManager):
         filter_name = 'name'
         filter_type = 'scalar'
         taggable = True
-        tag_resource_type = 'eventstreaming'
+        tag_resource_type = 'EGS_EVENTSTREAMING'
 
-    def list_resources(self, query=None):
-        """查询事件流列表"""
-        client = local_session(self.session_factory).client('eg')
-        try:
-            # 创建请求对象
-            request = ListEventStreamingRequest()
-            # 设置请求参数
-            if query:
-                for key, value in query.items():
-                    if hasattr(request, key) and value:
-                        setattr(request, key, value)
-            # 获取响应
-            response = client.list_event_streaming(request)
-            resources = response.items if response.items else []
-            for resource in resources:
-                # 确保每个资源都有id字段
-                resource['id'] = resource.get('id', '')
-                resource['tag_resource_type'] = self.resource_type.tag_resource_type
+    def augment(self, resources):
+        """Augment resources with tag information.
+
+        :param resources: List of EventStreaming resource dictionaries.
+        :return: Augmented list of resources.
+        """
+        if not resources:
             return resources
-        except exceptions.ClientRequestException as e:
-            self.log.error(f"获取事件流列表失败: {e}")
-            return []
+            
+        # Attempt to create TMS client to query tags
+        try:
+            session = local_session(self.session_factory)
+            client = session.client('tms')
+            
+            for resource in resources:
+                try:
+                    request = ShowResourceTagRequest()
+                    request.resource_id = resource['id']
+                    request.resource_type = self.resource_type.tag_resource_type
+                    
+                    response = client.show_resource_tag(request)
+                    
+                    # Format tags into the expected structure
+                    if hasattr(response, 'tags') and response.tags is not None:
+                        tags = []
+                        for tag in response.tags:
+                            tags.append({
+                                'key': tag.key,
+                                'value': tag.value
+                            })
+                        resource['tags'] = tags
+                    else:
+                        resource['tags'] = []
+                except exceptions.ClientRequestException as e:
+                    self.log.warning(
+                        f"Failed to retrieve tags for EventStreaming {resource['id']}: "
+                        f"{e.error_code} - {e.error_msg}")
+                    # Do not modify the resource or set empty tags on client exception
+        except Exception as e:
+            self.log.error(f"Error during tag augmentation: {str(e)}")
+            # Return original resources if any error occurs during the process
+            
+        return resources
 
 
 @EventStreaming.filter_registry.register('age')
 class EventStreamingAgeFilter(AgeFilter):
-    """事件流创建时间过滤器
+    """Filters EventStreaming resources based on their creation time.
     
     :example:
 
@@ -91,8 +108,3 @@ class EventStreamingAgeFilter(AgeFilter):
         minutes={'type': 'number'}
     )
     date_attribute = "created_time"
-
-
-# 为EventStreaming资源注册标签过滤器
-EventStreaming.filter_registry.register('tag', TagCountFilter)
-EventStreaming.filter_registry.register('tag-action', TagActionFilter)
