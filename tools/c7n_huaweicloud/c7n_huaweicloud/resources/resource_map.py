108eed3d191bbe7d9aedadb9b8d4868039e5f84e
franklwy <39915330+franklwy@users.noreply.github.com>
2025-05-08 12:26:15 +0800

    Update resource_map.py

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/resource_map.py
@@ -42,6 +42,9 @@ ResourceMap = {
     "huaweicloud.ram-shared-principals": "c7n_huaweicloud.resources.ram.RAMSharedPrincipals",
     "huaweicloud.antiddos-eip": "c7n_huaweicloud.resources.antiddos.Eip",
     "huaweicloud.kafka": "c7n_huaweicloud.resources.kafka.Kafka",
+    "huaweicloud.dns-publiczone": "c7n_huaweicloud.resources.dns.PublicZone",
+    "huaweicloud.dns-privatezone": "c7n_huaweicloud.resources.dns.PrivateZone",
+    "huaweicloud.dns-recordset": "c7n_huaweicloud.resources.dns.RecordSet",
     "huaweicloud.rds": "c7n_huaweicloud.resources.rds.RDS",
     "huaweicloud.rds-mysql-database": "c7n_huaweicloud.resources.rds_mysql.RDSMySQLDatabase",
     "huaweicloud.rds-mysql-user": "c7n_huaweicloud.resources.rds_mysql.RDSMySQLUser"
