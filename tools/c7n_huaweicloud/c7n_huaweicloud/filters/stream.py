e9004653177f54794a2ad703cdf7ee4f83bdebf6
tangtao40huawei <tangtao40@huawei.com>
2025-06-05 14:59:36 +0800

    add mode

--- tools/c7n_huaweicloud/c7n_huaweicloud/filters/stream.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/filters/stream.py
@@ -20,6 +20,36 @@ class LtsStreamStorageEnabledFilter(Filter):
     def process(self, resources, event=None):
         client = self.manager.get_client()
         request = ListLogStreamRequest()
+        log.error("enter lts filter")
+        streams = []
+        for group in resources:
+            request.log_group_id = group.log_group_id
+            try:
+                time.sleep(0.3)
+                response = client.list_log_stream(request)
+                for stream in response.log_streams:
+                    if stream.whether_log_storage:
+                        streamDict = {}
+                        streamDict["log_group_id"] = group.log_group_id
+                        streamDict["log_stream_id"] = stream.log_stream_id
+                        streamDict["log_stream_name"] = stream.log_stream_name
+                        streams.append(streamDict)
+            except Exception as e:
+                log.error(e)
+                continue
+        log.error("The number of streams to disable storage is " + str(len(streams)))
+        return streams
+
+
+class LtsStreamStorageEnabledFilterForSchedule(Filter):
+    schema = type_schema(
+        'streams-storage-enabled-for-schedule'
+    )
+
+    def process(self, resources, event=None):
+        client = self.manager.get_client()
+        request = ListLogStreamRequest()
+        log.error("enter lts filter for schedule")
         streams = []
         for group in resources:
             request.log_group_id = group["log_group_id"]
@@ -31,10 +61,10 @@ class LtsStreamStorageEnabledFilter(Filter):
                         streamDict = {}
                         streamDict["log_group_id"] = group["log_group_id"]
                         streamDict["log_stream_id"] = stream.log_stream_id
-                        streamDict["log_stream_name"] = stream.log_stream_name
+                        streamDict["log_group_name"] = stream.log_stream_name
                         streams.append(streamDict)
             except Exception as e:
                 log.error(e)
                 continue
-        log.error("The number of streams to disable storage is " + str(len(streams)))
+        log.error("The number of streams to disable storage is" + str(len(streams)))
         return streams
