b3981523cc4f2a0c787bcaf095a5c7c7f583087d
nizhaobuzhao <zhaobuzhao888@163.com>
2025-06-16 14:12:35 +0800

    lint check

--- tools/c7n_huaweicloud/c7n_huaweicloud/actions/elb.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/actions/elb.py
@@ -429,10 +429,10 @@ class ListenerRedirectAction(HuaweiCloudBaseAction):
 
 def check_elb_resource(response):
     if response is None:
-      log.error("Failed to get response from ELB service")
-      raise exceptions.ClientRequestException()
+        log.error("Failed to get response from ELB service")
+        raise exceptions.ClientRequestException()
     if response.status_code != 200 and response.status_code != 201 and \
-      response.status_code != 204:
-      log.error(response.status_code, response.request_id,
-                response.error_code, response.error_msg)
-      raise exceptions.ClientRequestException()
+        response.status_code != 204:
+        log.error(response.status_code, response.request_id,
+                  response.error_code, response.error_msg)
+        raise exceptions.ClientRequestException()
