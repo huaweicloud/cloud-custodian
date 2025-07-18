6850ff1307383d8746cc656ffbe14fd289f372e2
tangtao40huawei <tangtao40@huawei.com>
2025-07-18 18:01:23 +0800

    add logs

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/stream.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/stream.py
@@ -48,6 +48,8 @@ class Stream(QueryResourceManager):
                         streamDict["log_group_id"] = group.log_group_id
                         streamDict["log_stream_id"] = stream.log_stream_id
                         streamDict["log_stream_name"] = stream.log_stream_name
+                        streamDict["id"] = stream.log_stream_id
+                        streamDict["tags"] = stream.tag
                         streams.append(streamDict)
                         should_break = True
                         break
