79e819cda7a70b6117676bde3b9b0a73c1290539
zhangshun523 <v_523994191@qq.com>
2025-07-23 11:41:01 +0800

    cbr optimization log records.

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/cbr_protectable.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/cbr_protectable.py
@@ -172,7 +172,7 @@ class CbrAssociateServerVault(HuaweiCloudBaseAction):
                               f"{vaults[vault_num]['id']} success.")
             except exceptions.ClientRequestException as e:
                 log.error(f"[actions]-[{self.action_name}] "
-                          f"failed to add resource to {vaults[vault_num]['id']}, "
+                          f"add resource to vault id:{vaults[vault_num]['id']} failed, "
                           f"cause request id:{e.request_id}, msg:{e.error_msg}")
                 raise
             vault_num += 1
