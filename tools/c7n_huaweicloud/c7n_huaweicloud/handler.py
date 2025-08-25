dbf9c7542270343fff3ebf2c01ecc4ae3cb65a80
yufanxuan <yufanxuan@126.com>
2025-08-25 18:46:13 +0800

    huaweicloud - FunctionGraph support region in policy condition

--- tools/c7n_huaweicloud/c7n_huaweicloud/handler.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/handler.py
@@ -45,6 +45,7 @@ def run(event, context=None):
     options_overrides['domain_id'] = context.getUserData('DOMAIN_ID')
     options_overrides['account_id'] = context.getUserData('DOMAIN_ID')
     options_overrides['account_name'] = context.getUserData('DOMAIN_NAME')
+    options_overrides['region'] = context.getUserData('HUAWEI_DEFAULT_REGION')
 
     # merge all our options in
     options = Config.empty(**options_overrides)
